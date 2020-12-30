c2e801ca9b45f0055ca571dbcccbc3047ed3fc2d
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-12-29 20:56:13 -0600

    fix: export reusable config

--- index.js
+++ index.js
@@ -43,6 +43,48 @@ function postprocess(messages, filename) {
 require('eslint-plugin-html');
 
 module.exports = {
+  configs: {
+    config: {
+      parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: 'module',
+      },
+      plugins: ['fruition'],
+      env: {
+        es6: true,
+        browser: true,
+      },
+      rules: {
+        indent: 0,
+        camelcase: 0,
+        'function-paren-newline': 0,
+        'arrow-body-style': 0,
+        'consistent-return': 0,
+        'global-require': 0,
+        'no-labels': 0,
+        'no-console': 0,
+        'no-multi-assign': 0,
+        'no-unused-labels': 0,
+        'no-restricted-syntax': 0,
+        'no-underscore-dangle': 0,
+        'no-param-reassign': 0,
+        'no-restricted-globals': 0,
+        'prefer-destructuring': 0,
+        'prefer-spread': 0,
+        'prefer-const': 0,
+        'prefer-rest-params': 0,
+        'prefer-arrow-callback': 0,
+        'import/first': 0,
+        'import/extensions': 0,
+        'import/no-extraneous-dependencies': 0,
+        'import/no-dynamic-require': 0,
+        'import/no-unresolved': 0,
+        'import/no-mutable-exports': 0,
+        'import/prefer-default-export': 0,
+        'arrow-parens': ['error', 'as-needed'],
+      },
+    },
+  },
   processors: {
     '.html': {
       preprocess,
