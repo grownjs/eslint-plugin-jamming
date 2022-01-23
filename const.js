1d2ba475f84dd7cd9a08c44b96b60fea21cae376
Alvaro Cabrera <pateketrueke@gmail.com>
2022-01-23 13:28:32 -0600

    fix: extract names from spread syntax

--- const.js
+++ const.js
@@ -7,7 +7,7 @@ const RE_MATCH_QUOTED = /(["'])(\w+)\1/;
 const RE_CONTEXT_MODULE = /\scontext=(["'])?module\1/;
 const RE_TYPE_MODULE = / type=(["'])?module\1/;
 const RE_SPLIT_MARKER = /_(\d+)_(\d+):(\w+)/;
-const RE_ACCESED_SYMBOLS = /(?:(?:(?<=[=([]\s*)[_$a-zA-Z]\w*|(?<![.#]\w*)[_$a-zA-Z]\w*)(?= *[.,;\n[})\]|&])|(?<![.#]\w*)[_$a-zA-Z]\w*(?= *[(?:!<=>/*+-]{1,3}| *(?:in|as) *))/; // eslint-disable-line
+const RE_ACCESED_SYMBOLS = /(?:(?:(?<=[=([]\s*|\.\.\.)[_$a-zA-Z]\w*|(?<![.#]\w*)[_$a-zA-Z]\w*)(?= *[.,;\n[})\]|&])|(?<![.#]\w*)[_$a-zA-Z]\w*(?= *[(?:!<=>/*+-]{1,3}| *(?:in|as) *))/; // eslint-disable-line
 const RE_COMMENT_INLINE = /(?!:) *\/\/ +.*?(?=\n)|\/\*[^]*?\*\//g;
 const RE_COMMENT_SAFE = /<\/|(?<=\/[*/] *)(?:eslint|global)\b(?=[ \w,-]+)/;
 const RE_EXPORT_IMPORT = /\bexport +|import[^]+?[\n;]/g;
